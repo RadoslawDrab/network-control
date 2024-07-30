99af603f43c6cee86fb9e024f9f4b2baf33d4e4e
RadoslawDrab <radoslaw.drab03@gmail.com>
2024-07-30 13:57:05 +0200

    added app utils

---
+++ src/utils/index.ts
@@ -0,0 +1,20 @@
+import { Request } from 'express';
+
+import { getStatus } from './server';
+
+export function getAddresses(req: Request) {
+  const address = req.headers['mac_address'];
+  const errorStatus = getStatus(400, 'MAC address is required');
+
+  if (!address) throw errorStatus;
+  const macRegEx = new RegExp(/[0-9A-FX]{12}/i);
+  const macFilter = (addr: string) => !!addr.match(macRegEx);
+
+  const addresses = (Array.isArray(address) ? address : [...address.replace(/ /g, '').split(',')]).map((addr) =>
+    addr.replace(/\W/g, '').toUpperCase()
+  );
+
+  if (addresses.length === 0) throw errorStatus;
+  if (!addresses.find(macFilter)) throw getStatus(400, 'No valid MAC address');
+  return addresses.filter(macFilter);
+}
