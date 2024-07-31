b6f9100e7d60ee959046e8450231ab92fe035f29
RadoslawDrab <radoslaw.drab03@gmail.com>
2024-07-31 08:52:14 +0200

    added checking admin

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,15 +1,27 @@
 import express from 'express';
 
-import { getAddresses } from 'utils/index';
+import { getAddresses, checkAdmin as admin } from 'utils/index';
 import { setStatus } from 'utils/server';
 
 import { Status } from 'types/server';
+import { AppConfig } from 'types';
 
-export default function (req: express.Request, res: express.Response, next: express.NextFunction) {
+export function checkAddress(req: express.Request, res: express.Response, next: express.NextFunction) {
   try {
     getAddresses(req);
     next();
   } catch (error) {
     return setStatus(res, { code: 400, message: (error as Status).message });
+  }
+}
+export function checkAdmin(this: AppConfig, req: express.Request, res: express.Response, next: express.NextFunction) {
+  try {
+    const containsAdmin = admin(req, this);
+
+    // Checks if request contains admin address
+    if (containsAdmin) next();
+    else setStatus(res, { code: 401, message: 'Invalid admin MAC address' });
+  } catch (error) {
+    setStatus(res, error as Status);
   }
 }
