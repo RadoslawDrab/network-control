46169d6e2a32dc27aa6f76f5a23b25614e3f907a
RadoslawDrab <radoslaw.drab03@gmail.com>
2024-07-31 08:52:58 +0200

    changed getting address from header to param

--- src/routes/status.ts
+++ src/routes/status.ts
@@ -1,40 +1,56 @@
 import express from 'express';
 import { AppConfig } from 'types/index';
 
-import { getAddresses } from 'utils/index';
+import { checkAddressesValidity, standarizeAddresses } from 'utils/index';
 import { setStatus } from 'utils/server';
+import { checkAddress, checkAdmin } from 'middleware';
 
 export default (config: AppConfig) => {
   const router = express.Router();
+  router.get('/:address', (req, res) => {
+    // Addresses from param
+    const address = standarizeAddresses([req.params.address])[0];
+
+    if (!checkAddressesValidity(address)[0]) return setStatus(res, { code: 400, message: 'Invalid MAC address' });
+    const currentTime = Date.now();
+
+    // Addresses saved in file
+    const savedAddresses = config.get().addresses ?? [];
+    // Same addresses from param and file
+    const filteredAddresses = savedAddresses.filter((savedAddress) => savedAddress.address === address);
+
+    if (filteredAddresses.length === 0) return setStatus(res, { code: 404, message: 'MAC address not found' });
+
+    // Lock times from all addresses
+    const lockTimes = filteredAddresses.map((addr) => addr.lockAfter).filter((time) => time && time > 0) as number[];
+
+    // Longest lock time
+    const lockTime = Math.max(...lockTimes);
+
+    res.status(200).json({
+      lockAfter: lockTime,
+      isLocked: currentTime >= lockTime,
+      requestTime: currentTime,
+      remainingSeconds: Math.max(Math.round((lockTime - currentTime) / 1000), 0),
+    });
+  });
   router
-    .route('/status')
-    .get((req, res) => {
-      const addresses = getAddresses(req);
-      const currentTime = Date.now();
-
-      const savedAddresses = config.get().addresses ?? [];
-      const filteredAddresses = savedAddresses.filter((savedAddress) =>
-        addresses.find((addr) => addr === savedAddress.address)
-      );
-
-      if (filteredAddresses.length === 0) return setStatus(res, { code: 404, message: 'MAC address not found' });
-
-      const lockTimes = filteredAddresses.map((addr) => addr.lockAfter).filter((time) => time && time > 0) as number[];
-
-      const lockTime = Math.max(...lockTimes);
-
-      res.status(200).json({
-        lockAfter: lockTime,
-        isLocked: currentTime >= lockTime,
-        requestTime: currentTime,
-        remainingSeconds: Math.max(Math.round((lockTime - currentTime) / 1000), 0),
-      });
-    })
-    .post((req, res) => {
-      const addresses = getAddresses(req);
+    .use(checkAddress)
+    .use(checkAdmin.bind(config))
+    .post('/:address', (req, res) => {
       const currentTime = Date.now();
 
       const { hours, minutes, seconds, time: t } = req.body as Record<string, number | undefined>;
+      const address = standarizeAddresses([req.params.address ?? ''])[0] as string | undefined;
+
+      if (!address) return setStatus(res, { code: 400, message: 'Target not specified' });
+
+      // Admin addresses from file
+      const adminAddresses = config.get().adminAddresses ?? [];
+
+      // Checks if user wants to add time to admin address
+      if (adminAddresses.includes(address)) return setStatus(res, 400);
+
       // Gets time from hours, minutes and seconds combined unless `time` variable is present
       const time = t ?? (hours ?? 0) * 60 * 60 * 1000 + (minutes ?? 0) * 60 * 1000 + (seconds ?? 0) * 1000;
 
@@ -56,30 +72,28 @@ export default (config: AppConfig) => {
         !!seconds?.toString().match(plusRegEx) ||
         !!time.toString().match(plusRegEx);
 
-      const newAddresses = addresses.map((addr) => {
-        const lockAfter =
-          savedAddresses.find((savedAddress) => addr === savedAddress.address)?.lockAfter ?? currentTime;
+      const currentlockAfter =
+        savedAddresses.find((savedAddress) => address === savedAddress.address)?.lockAfter ?? currentTime;
+
+      const lockAfter = (() => {
         // Changes lock to current time if `time` is 0
-        if (changeTime === 0) return { address: addr, lockAfter: currentTime };
-        return {
-          address: addr,
-          lockAfter:
-            changeTime > 0
-              ? // Checks if user wants to add time to lock time or change it
-                addTime
-                ? // Adds to current time
-                  Math.max(lockAfter, currentTime) + changeTime
-                : // Changes `lockAfter`
-                  currentTime + changeTime
-              : // Removes time
-                lockAfter + changeTime,
-        };
-      });
+        if (changeTime === 0) return currentTime;
+        return changeTime > 0
+          ? // Checks if user wants to add time to lock time or change it
+            addTime
+            ? // Adds to current time
+              Math.max(currentlockAfter, currentTime) + changeTime
+            : // Changes `lockAfter`
+              currentTime + changeTime
+          : // Removes time
+            currentlockAfter + changeTime;
+      })();
+
       config.set({
         addresses: [
           // Filters out addresses present in header
-          ...savedAddresses.filter((savedAddress) => !addresses.find((addr) => addr === savedAddress.address)),
-          ...newAddresses,
+          ...savedAddresses.filter((savedAddress) => address !== savedAddress.address),
+          { address, lockAfter },
         ],
       });
 
