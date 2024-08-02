803a42187bd0b592441b3363d3e796519e01f724
RadoslawDrab <radoslaw.drab03@gmail.com>
2024-08-02 12:37:31 +0200

    added type key

--- env.d.ts
+++ env.d.ts
@@ -2,6 +2,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       NODE_ENV?: 'development' | 'production';
+      _HOSTNAME: string;
       _PORT: number;
       _ENC_KEY?: string;
     }
