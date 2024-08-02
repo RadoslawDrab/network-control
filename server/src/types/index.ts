34f69903aafcc42ccbe6d49a08e33dc0d73b2bd0
RadoslawDrab <radoslaw.drab03@gmail.com>
2024-08-02 15:21:54 +0200

    added new config types

--- server/src/types/index.ts
+++ server/src/types/index.ts
@@ -7,5 +7,11 @@ export type SettingsKey = keyof Settings;
 export interface Settings extends ConfigType {
   addresses: { address: string; lockAfter?: number }[];
   adminAddresses: string[];
+  /** Time in miliseconds after which info will be closed */
+  showTimeInfoTill: number;
+  /** Time in seconds to keep info after reminder */
+  showTimeInfoDuration: number;
+  /** Time in seconds to show reminder */
+  reminderTime: number;
 }
 export type AppConfig = Config<Settings>;
