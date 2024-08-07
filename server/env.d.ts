83ab7b7fd97ce240f68b2d91d8b75392494e2010
RadoslawDrab <radoslaw.drab03@gmail.com>
2024-08-07 12:25:57 +0200

    modified env types

--- server/env.d.ts
+++ server/env.d.ts
@@ -1,10 +1,11 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      NODE_ENV?: 'development' | 'production';
+      NODE_ENV: 'development' | 'production';
       _HOSTNAME: string;
       _PORT: number;
-      _ENC_KEY?: string;
+      _ENC_KEY: string;
+      _ADMIN_PASSWORD: string;
     }
   }
 }
