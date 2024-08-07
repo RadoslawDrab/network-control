b6117d40dd20fa1d354deeb69eea930735d33eb3
RadoslawDrab <radoslaw.drab03@gmail.com>
2024-08-07 12:00:26 +0200

    added admin password checking

--- server/src/types/index.ts
+++ server/src/types/index.ts
@@ -13,5 +13,8 @@ export interface Settings extends ConfigType {
   showTimeInfoDuration: number;
   /** Time in seconds to show reminder */
   reminderTime: number;
+  adminPassword: string;
+  /** Duration for which password will be kept */
+  adminPasswordCacheTime: number;
 }
 export type AppConfig = Config<Settings>;
