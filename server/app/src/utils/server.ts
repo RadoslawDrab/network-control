54c2de1d829de4fdb1f3382bb7f48b6386718bc2
RadoslawDrab <radoslaw.drab03@gmail.com>
2024-08-13 11:28:08 +0200

    added query checking and default method

--- server/app/src/utils/server.ts
+++ server/app/src/utils/server.ts
@@ -8,10 +8,13 @@ export async function promise<
   init?: RequestInit,
   baseUrl: string = import.meta.env.DEV ? 'http://localhost:3000/api' : '/api'
 ): Promise<Data> {
-  const queryString = query
-    ? Object.keys(query).reduce((str, key, i) => (str += `${i > 0 ? '&' : ''}${key}=${query[key]}`), '?')
-    : '';
+  const queryString =
+    query && Object.keys(query).length > 0
+      ? Object.keys(query).reduce((str, key, i) => (str += `${i > 0 ? '&' : ''}${key}=${query[key]}`), '?')
+      : '';
+
   const response = await fetch(baseUrl + path.replace(/\/$/, '') + queryString, {
+    method: 'GET',
     ...init,
     headers: { 'Content-Type': 'application/json', ...init.headers },
   });
