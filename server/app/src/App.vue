13c313b98aa1dd61925fdd1a72c685d696a42be6
RadoslawDrab <radoslaw.drab03@gmail.com>
2024-08-13 11:32:20 +0200

    added App

--- server/app/src/App.vue
+++ server/app/src/App.vue
@@ -1,4 +1,37 @@
-<script setup lang="ts"></script>
+<script setup lang="tsx">
+import { reactive, ref } from 'vue';
+
+import { NavItem } from 'components/MainHeader.vue';
+
+const show = reactive<{ changePasswordModal: boolean; addDeviceForm: boolean }>({
+  changePasswordModal: false,
+  addDeviceForm: false,
+});
+
+const navItems = ref<NavItem<keyof typeof show>[]>([
+  {
+    id: 'addDeviceForm',
+    html: 'Dodaj komputer',
+    passwordRequired: true,
+  },
+  { id: 'changePasswordModal', html: 'Zmień hasło', passwordRequired: true },
+]);
+
+function navCallback(id: string) {
+  Object.keys(show)
+    .filter((key) => key !== id)
+    .forEach((key) => {
+      show[key] = false;
+    });
+  show[id] = true;
+}
+</script>
 <template>
-  <div>APP</div>
+  <BContainer>
+    <MainHeader :navItems="navItems" :callback="navCallback" />
+    <div>APP</div>
+  </BContainer>
+  <PasswordModal new-password v-model:show="show.changePasswordModal" />
+  <DeviceFormOffcanvas v-model="show.addDeviceForm" @submit="(settings) => console.log(settings)" />
+  <BToastOrchestrator />
 </template>
