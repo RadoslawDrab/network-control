6e16a48180299740d3cc0bdbf04f6519e4355a62
RadoslawDrab <radoslaw.drab03@gmail.com>
2024-08-13 11:26:33 +0200

    added form feedback composable

---
+++ server/app/src/composables/useFeedback.ts
@@ -0,0 +1,30 @@
+import { computed, reactive, ref, toValue } from 'vue';
+
+const useFeedback = <T extends Record<string, any>>(
+  defaultValue: Partial<T>,
+  validity?: (values: Partial<T>) => Partial<Record<keyof T, boolean>>
+) => {
+  const value = reactive<Partial<T>>(defaultValue);
+  const isTouched = ref<Partial<Record<keyof T, boolean>>>({});
+  const isValid = computed<Partial<Record<keyof T, boolean | null>>>(() => {
+    if (!validity) return {};
+
+    const valid = validity(toValue(value));
+    return Object.keys(valid).reduce((obj, key) => {
+      return { ...obj, [key]: isTouched.value && isTouched.value[key] ? valid[key] : null };
+    }, valid);
+  });
+
+  function onTouched(name: keyof T) {
+    // @ts-expect-error Is correct key
+    isTouched.value[name] = true;
+  }
+
+  function reset() {
+    Object.assign(value, defaultValue);
+    isTouched.value = {};
+  }
+
+  return { onTouched, value, isValid, isTouched, v: value, reset };
+};
+export default useFeedback;
