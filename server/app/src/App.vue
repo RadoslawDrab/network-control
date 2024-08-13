c28228edcb6d6e999ba8ddc79fa7e13c61748346
RadoslawDrab <radoslaw.drab03@gmail.com>
2024-08-13 11:48:35 +0200

    added device adding

--- server/app/src/App.vue
+++ server/app/src/App.vue
@@ -1,6 +1,11 @@
 <script setup lang="tsx">
 import { reactive, ref } from 'vue';
 
+import usePromiseAuth from 'composables/usePromiseAuth';
+import useToast from 'composables/useToast';
+
+import { Address } from 'types/index';
+
 import { NavItem } from 'components/MainHeader.vue';
 
 const show = reactive<{ changePasswordModal: boolean; addDeviceForm: boolean }>({
@@ -17,6 +22,9 @@ const navItems = ref<NavItem<keyof typeof show>[]>([
   { id: 'changePasswordModal', html: 'Zmień hasło', passwordRequired: true },
 ]);
 
+const auth = usePromiseAuth();
+const toast = useToast();
+
 function navCallback(id: string) {
   Object.keys(show)
     .filter((key) => key !== id)
@@ -25,6 +33,15 @@ function navCallback(id: string) {
     });
   show[id] = true;
 }
+
+async function onDeviceAdded(settings: Address) {
+  try {
+    await auth.promise('/user', {}, { method: 'POST', body: JSON.stringify(settings) });
+    toast.show('Device added', { variant: 'success' });
+  } catch (error) {
+    toast.show('Failed', { variant: 'danger', body: error.message });
+  }
+}
 </script>
 <template>
   <BContainer>
@@ -32,6 +49,6 @@ function navCallback(id: string) {
     <div>APP</div>
   </BContainer>
   <PasswordModal new-password v-model:show="show.changePasswordModal" />
-  <DeviceFormOffcanvas v-model="show.addDeviceForm" @submit="(settings) => console.log(settings)" />
+  <DeviceFormOffcanvas v-model="show.addDeviceForm" @submit="onDeviceAdded" />
   <BToastOrchestrator />
 </template>
