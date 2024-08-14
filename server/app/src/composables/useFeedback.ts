5fb7887748f3a9a1730a65dffa84aac7b2a7e19d
RadoslawDrab <radoslaw.drab03@gmail.com>
2024-08-14 11:54:55 +0200

    added checking all validity

--- server/app/src/composables/useFeedback.ts
+++ server/app/src/composables/useFeedback.ts
@@ -2,10 +2,12 @@ import { computed, nextTick, reactive, ref, toValue } from 'vue';
 
 const useFeedback = <T extends Record<string, any>>(
   defaultValue: Partial<T>,
-  validity?: (values: Partial<T>) => Partial<Record<keyof T, boolean>>
+  validity?: (values: Partial<T>) => Partial<Record<keyof T, boolean>>,
+  isTouchedAlready: (keyof T)[] = []
 ) => {
+  const isTouchedAlreadyObj = isTouchedAlready.reduce((obj, key) => ({ ...obj, [key]: true }), {});
   const value = reactive<Partial<T>>({ ...defaultValue });
-  const isTouched = ref<Partial<Record<keyof T, boolean>>>({});
+  const isTouched = ref<Partial<Record<keyof T, boolean>>>(isTouchedAlreadyObj);
   const isValid = computed<Partial<Record<keyof T, boolean | null>>>(() => {
     if (!validity) return {};
 
@@ -13,6 +15,9 @@ const useFeedback = <T extends Record<string, any>>(
     return Object.keys(valid).reduce((obj, key) => {
       return { ...obj, [key]: isTouched.value && isTouched.value[key] ? valid[key] : null };
     }, valid);
+  });
+  const allValid = computed<boolean>(() => {
+    return Object.keys(isValid.value).every((key) => isValid.value[key] === true);
   });
 
   function onTouched(name: keyof T) {
@@ -26,6 +31,6 @@ const useFeedback = <T extends Record<string, any>>(
     await nextTick();
   }
 
-  return { onTouched, value, isValid, isTouched, v: value, reset };
+  return { onTouched, value, isValid, isTouched, v: value, reset, allValid };
 };
 export default useFeedback;
