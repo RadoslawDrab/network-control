ab56d333f861b24165c39948055a0741d5525fab
RadoslawDrab <radoslaw.drab03@gmail.com>
2024-09-27 10:13:54 +0200

    added or function

--- server/app/src/utils/logic.ts
+++ server/app/src/utils/logic.ts
@@ -2,13 +2,25 @@ export function and<T extends string | boolean | number>(
   value: T,
   ...values: (T extends string ? string | RegExp : T)[]
 ) {
-  return values.every((v) => {
-    switch (typeof value) {
-      case 'string':
-        const matcher = v as RegExp | string;
-        return value.match(matcher).some((v) => v !== '');
-      default:
-        return value === v;
-    }
-  });
+  return values.every(handler.bind(value));
+}
+export function or<T extends string | boolean | number>(
+  value: T,
+  ...values: (T extends string ? string | RegExp : T)[]
+) {
+  return values.some(handler.bind(value));
+}
+function handler<T extends string | boolean | number>(
+  this: T,
+  value: T extends string ? string | RegExp : T,
+  index: number,
+  array: (T extends string ? string | RegExp : T)[]
+) {
+  switch (typeof this) {
+    case 'string':
+      const matcher = value as RegExp | string;
+      return this.match(matcher).some((v) => v !== '');
+    default:
+      return this === value;
+  }
 }
