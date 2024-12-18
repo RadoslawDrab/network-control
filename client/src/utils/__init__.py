a14c9504d7d2e1682dcbc2ae3e2210474ed98648
RadoslawDrab <radoslaw.drab03@gmail.com>
2024-12-18 12:20:10 +0100

    added getting types and object attributes

--- client/src/utils/__init__.py
+++ client/src/utils/__init__.py
@@ -1,4 +1,6 @@
 import threading
+import re
+from typing import Callable, TypeVar
 
 def runInParallel(*fns):
   threads: list[threading.Thread] = []
@@ -11,3 +13,22 @@ def runInParallel(*fns):
 
 def enum(**enums):
     return type('Enum', (), enums)
+
+def get_attributes(obj: object) -> list[str]:
+  return [attribute for attribute in dir(obj) if not attribute.startswith('_') and not isinstance(getattr(obj, attribute), Callable)]
+
+T = TypeVar('T')
+def get_type[T](value: any) -> T:
+  if re.match(r'true|false', value, re.I):
+    return bool(value)
+  if re.match(r'^\d+$', value):
+    return int(value)
+  if re.match(r'^[0-9.,]+$', value):
+    return float(value)
+  if re.match(r'^\[.*\]', value):
+    value_list = list(re.split(',', re.sub(r'\[|\]', '', value)))
+    return [get_type(item.strip()) for item in value_list]
+  if value == 'None':
+    return None
+  
+  return str(value)
